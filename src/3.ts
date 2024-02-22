64dbac2ed1ce5f46e5ed8f8a63ffdf177ec9b897
sivlvy <vladislavsidenko@ukr.net>
2024-02-22 17:20:31 +0200

    update

--- src/3.ts
+++ src/3.ts
@@ -7,33 +7,44 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
+interface ICharacter {
+	name: string;
+	level: number;
+	introduce(phrase: string): void;
+	levelUp(): void;
+}
+
+interface ISpellCaster {
+	castSpell(): void;
+}
+
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
-  constructor(public name: string, public level: number) {
-    if (this.level < 1) {
-      throw new Error('Level too low');
-    }
-  }
+	constructor(public name: string, public level: number) {
+		if (this.level < 1) {
+			throw new Error("Level too low");
+		}
+	}
 
-  introduce(phrase: string): void {
-    console.log(phrase + ', ' + this.name);
-  }
+	introduce(phrase: string): void {
+		console.log(phrase + ", " + this.name);
+	}
 
-  castSpell(): void {
-    console.log('Casting a spell, behold my power!');
-  }
+	castSpell(): void {
+		console.log("Casting a spell, behold my power!");
+	}
 
-  levelUp(): void {
-    this.level++;
-    console.log(`Level up! New level is ${this.level}`);
-  }
+	levelUp(): void {
+		this.level++;
+		console.log(`Level up! New level is ${this.level}`);
+	}
 }
 
 // тестування класу
-const wizard = new Wizard('Merlin', 15);
+const wizard = new Wizard("Merlin", 15);
 
-wizard.introduce('I am the mighty wizard');
+wizard.introduce("I am the mighty wizard");
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp(); // Level up! New level is 16
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+class Key {
+	private signature: number;
+
+	constructor() {
+		this.signature = Math.random();
+	}
+
+	getSignature(): number {
+		return this.signature;
+	}
+}
+
+class Person {
+	private key: Key;
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	getKey(): Key {
+		return this.key;
+	}
+}
+
+abstract class House {
+	protected door: boolean;
+	protected key: Key;
+	protected tenants: Person[];
+
+	constructor(key: Key) {
+		this.door = false;
+		this.key = key;
+		this.tenants = [];
+	}
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person);
+			console.log(`Person ${person.getKey().getSignature()} entered the house.`);
+		} else {
+			console.log("Can't enter, the door is closed.");
+		}
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+			console.log("The door is opened");
+		} else {
+			console.log("Wrong key, the door remains closed.");
+		}
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +63,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
